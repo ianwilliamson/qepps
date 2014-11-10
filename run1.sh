a19c0f15e2ef593f4c7f27044bd7efc6c6d722de
Ian Williamson <ian.williamson@utexas.edu>
2014-11-10 17:31:16 -0600

    Updated run1.sh with corrected flags

--- run1.sh
+++ run1.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-ibrun -n 1 -o 0 \
-	./qepps -lambda_tgt 55+22i -qep_nev 1 -qep_tol 1e-6 \
+ibrun -n 1 -o 0 ./qepps \
+	-lambda_tgt 55+22i \
+	-lambda_tgt_i 22 \
+	-lambda_tgt_r 55 \
+	-qep_nev 1 \
+	-qep_ncv 20 \
+	-qep_tol 1e-6 \
 	-st_type sinvert \
-	-qep_monitor -qep_type linear -qep_linear_cform 1 \
-	-qep_st_ksp_type gmres -qep_st_pc_type bjacobi -qep_eps_type krylovschur \
+	-qep_monitor \
+	-qep_type linear \
+	-qep_linear_cform 1 \
 	-params $WORK/data_comsol/freqs.txt \
 	-E0 $WORK/data_comsol/E.dat \
 	-D0 $WORK/data_comsol/D.dat \
 	-K0 $WORK/data_comsol/K.dat
 
-
+#-qep_linear_explicitmatrix -qep_nev 4 -qep_ncv 20 -qep_tol 1e-9 -qep_terse > ex17_1.tmp 2>&1;
