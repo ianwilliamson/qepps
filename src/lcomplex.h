acd07682f03529d390e00fc1990ce237f210ebd6
Ian Williamson <ian.ad.williamson@gmail.com>
2014-11-23 21:16:04 -0600

    Cleaned up lcomplex.h to remove functions that are not used externally

--- src/lcomplex.h
+++ src/lcomplex.h
@@ -1,14 +1,6 @@
-
-#define Complex	double complex
-
-static Complex Pget(lua_State *L, int i);
-
-static int pushcomplex(lua_State *L, Complex z);
-
-static int Leq(lua_State *L);
-
-static int Ltostring(lua_State *L);
+#ifndef LCOMPLEXH
+#define LCOMPLEXH
 
 LUALIB_API int luaopen_complex(lua_State *L);
 
-
+#endif
