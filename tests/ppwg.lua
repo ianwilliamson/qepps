d90a5744d3b543fc8f2417dfb2aacb44e68841d0
Ian Williamson <ian.ad.williamson@gmail.com>
2014-11-26 22:53:18 -0600

    Updated ppwg.lua for new options format

--- tests/ppwg.lua
+++ tests/ppwg.lua
@@ -1,14 +1,17 @@
-j=complex.I --For convenience 
-
-lambda_tgt = 1.4
+j=complex.I -- For convenience 
 
 parameters = {}
 for param = 0.1E12, 1.5E12, 0.05E12 do   parameters[#parameters+1] = param   end
 
-format_parameter = ""
-format_lambda    = ""
+-- Software Options
+options = {    lambda_tgt = 1.4,
+               output_dir = os.getenv("WORK").."/data_gr3d_base",
+        update_lambda_tgt = false, 
+         update_initspace = false,
+           save_solutions = false 
+           }
 
-DIR = os.getenv("WORK") .. "/data_ppwg_base"
+DIR = options['output_dir']
 Edat = {DIR .. "/E0.dat", DIR .. "/E1.dat", DIR .. "/E2.dat"}
 Ddat = {DIR .. "/D0.dat", DIR .. "/D1.dat", DIR .. "/D2.dat"}
 Kdat = {DIR .. "/K0.dat", DIR .. "/K1.dat", DIR .. "/K2.dat"}
