cabf45b23eff29c23af33d1f8806dc2fb9fd093a
Ian Williamson <ian.williamson@utexas.edu>
2014-12-10 20:08:16 -0600

    Improved plot utility

--- tools/plot.py
+++ tools/plot.py
@@ -4,24 +4,34 @@ from matplotlib import pyplot as plt
 from matplotlib import cm
 
 parser=argparse.ArgumentParser(description='QEPPS Plot Utility')
-parser.add_argument('files',nargs='+',help='Data text files to plot')
-parser.add_argument('-out',default='',help='Save filename')
-parser.add_argument('-col',type=int,default=1,help='number of data columns to plot')
+parser.add_argument('files',nargs='+',help='QEEPS output text file(s)')
+parser.add_argument('-o','--output',default='',help='Save filename, if not specified will be shown interactively')
+parser.add_argument('--imag',action='store_true',help='Flag specifying if the imaginary component of the eigenvalues should be plotted')
+parser.add_argument('-c','--columns',type=int,default=1,help='Number of data columns to be plotted')
 args=parser.parse_args()
 
+plt.style.use('https://enw.me/pyplot/ian.mplstyle')
 ax = plt.gca()
 for fn in args.files:
-  data=np.loadtxt(fn,comments='#',delimiter=', ',dtype=np.complexfloating)
-  freqs=np.real(data[:,0])
-  neff =data[:,1:args.col+1]
-  plt.plot( freqs,  np.real(neff), linewidth=2, )
-  plt.plot( freqs, -np.imag(neff), linewidth=2, linestyle='--', color=ax.lines[-1].get_color() )
+  data=np.loadtxt(fn,comments='#',delimiter=',',dtype=complex)
+  freqs=np.real(data[:,0])/1E12
+  neff =data[:,1:args.columns+1]
+  plt.plot( freqs,  np.abs(np.real(neff)))
+  if args.imag:
+    plt.plot( freqs, -np.imag(neff), linestyle='--',color=ax.lines[-1].get_color() )
+  
+  fmin=np.amin(freqs)
+  fmax=np.amax(freqs)
+  plt.xlim( (fmin,fmax) ) 
+  
 
-plt.xlabel('Frequency')
-plt.ylabel('n')
+plt.xlabel('Frequency (THz)')
+plt.ylabel('k/k_0')
 
-if args.out:
-  plt.savefig(args.out)
+plt.legend
+
+if args.output:
+  plt.savefig(args.output)
 else:
   plt.show()
 
